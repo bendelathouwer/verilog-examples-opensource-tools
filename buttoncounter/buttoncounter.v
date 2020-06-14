31769813e73f898b54aea36b8f8af60641a9f35f
Ben <bendelathouwer@users.noreply.github.com>
2020-06-14 23:09:31 +0200

    fouten er uit gehaald

--- buttoncounter/buttoncounter.v
+++ buttoncounter/buttoncounter.v
@@ -1,68 +1,40 @@
-  
- /* 
-      A
-     ---
-  F | G | B
-     ---
-  E |   | C
-     ---
-      D
-0     : 7'b1000000
-1     : 7'b1111001
-2     : 7'b0100100
-3     : 7'b0110000
-4     : 7'b0011001
-5     : 7'b0010010
-6     : 7'b0000010
-7     : 7'b1111000
-8     : 7'b0000000
-9     : 7'b0010000
-A     : 7'b0001000
-B     : 7'b0000011
-C     : 7'b1000110
-D     : 7'b0100001
-E     : 7'b0000110
-F     : 7'b0001110
-blank : 7b'1111111
-*/
-
-
-
-
-module buttoncounter (button, display );
+module buttoncounter (button, display,clk );
 	   
-	   input wire button;
-	   input wire  [6:0] display ;
-	   
-	   reg counter;
-	   
-	   if(button == 1)
-		 counter = counter + 1'b1 ;
-		 if (counter==15)
-			counter = 1'b0;
-	     end 
-	   end 
-	   case(counter)
-	   		case 0  : 7'b1000000;
-			case 1  : 7'b1111001;
-			case 2  : 7'b0100100;
-			case 3  : 7'b0110000;
-			case 4  : 7'b0011001;
-			case 5  : 7'b0010010;
-			case 6  : 7'b0000010;
-			case 7  : 7'b1111000;
-			case 8  : 7'b0000000;
-			case 9  : 7'b0010000;
-			case 10 : 7'b0001000;
-			case 11 : 7'b0000011;
-			case 12 : 7'b1000110;
-			case 13 : 7'b0100001;
-			case 14 : 7'b0000110;
-			case 15 : 7'b0001110;
-	    	default : 7'b1111111;
-	    endcase
-	   
-	   
-	   
-	   
+	  input wire  button   ;
+	  input wire  clk      ;
+	  output [6:0 ] display;
+	  reg	 [14:0] counter;
+    always @(posedge clk)
+	  	begin
+	   		if(button == 1)
+				begin
+		 			counter <= counter + 1'b1 ;
+				end
+			if (counter==15)
+				begin
+					counter <= 1'b0;
+				end 
+	    	
+		
+		case(counter)
+			
+	   			16'h0	  : display <= 7'b1000000 ;//0
+				16'h1	  : display <= 7'b1111001 ;//1
+				16'h2	  : display <= 7'b0100100 ;//2
+				16'h3	  : display <= 7'b0110000 ;//3
+				16'h4	  : display <= 7'b0011001 ;//4
+				16'h5	  : display <= 7'b0010010 ;//5
+				16'h6	  : display <= 7'b0000010 ;//6
+			 	16'h7	  : display <= 7'b1111000 ;//7
+			 	16'h9	  : display <= 7'b0010000 ;//9
+				16'hA	  : display <= 7'b0001000 ;//10
+			 	16'hB	  : display <= 7'b0000011 ;//11
+			 	16'hC     : display <= 7'b1000110 ;//12
+			 	16'hD     : display <= 7'b0100001 ;//13
+			 	16'hE     : display <= 7'b0000110 ;//14
+			 	16'hF	  : display <= 7'b0001110 ;//15
+	    	 	default   : display <= 7'b1111111 ;// blank 
+		    
+		endcase
+	end
 endmodule 
