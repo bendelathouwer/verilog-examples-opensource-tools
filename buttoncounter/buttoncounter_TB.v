8e45e77e4030ae0334aaa4be38089c5de6621872
Ben <bendelathouwer@users.noreply.github.com>
2020-06-20 20:19:27 +0200

    test bench
    debuggen test bench

--- buttoncounter/buttoncounter_TB.v
+++ buttoncounter/buttoncounter_TB.v
@@ -1,10 +1,31 @@
 `timescale 1ns/100ps
-module buttoncounter_TB;
+module buttoncounter_tb;
 reg button;
 reg clk
-wire [6:0] display;
+reg [6:0] display;
+reg	 [14:0] counter;
 buttoncounter u1 
 (
-        
-)
+    .clk(clk),
+    .button (button),
+    .display(display),
+    .counter(counter)
+);
+
+initial begin
+    $dumpfile ("buttoncounter_tb.vcd");
+    $dumpvars ( 0,buttoncounter_tb   );
+end
+    initial begin
+        clk = 0;
+        for(integer i = 0; i < 8; i = i + 1)
+        begin
+            #82 clk = ~ clk;
+        end
+        for(integer j = 0 ; j < 8 ; j = j + 1)
+        begin
+            button = ~ button;
+        end
+    end
+end
 endmodule
