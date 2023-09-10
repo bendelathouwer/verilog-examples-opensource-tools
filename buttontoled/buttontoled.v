d9109cf8255dedbd06fe34f00fa2c2c5b53b8159
Ben <bendelathouwer@users.noreply.github.com>
2023-09-10 20:32:13 +0200

    hardware.json
    generated the hardware.json file

--- buttontoled/buttontoled.v
+++ buttontoled/buttontoled.v
@@ -4,7 +4,7 @@ module buttontoled (i_sw, o_led);
     output reg o_led;
     always@*
     begin
-        assign o_led = i_sw ;//connects the led to the switch 
-    end 
+         o_led = i_sw ;//connects the led to the switch 
+    end  
 
 endmodule
