9a326cfb30f3865a588b0c8fdf838ee44b53ca16
Ben <bendelathouwer@users.noreply.github.com>
2023-09-16 18:25:32 +0200

    Update clockdevider_tb.v

--- clockdivider /clockdevider_tb.v
+++ clockdivider /clockdevider_tb.v
@@ -1,7 +1,11 @@
 `timescale 1ns/1ps
 module clockdevider_tb(output wire o_led);
-input wire clk,i_reset
-
-
-
+reg i_clk,i_reset,o_couterout;
+clockdivider UUT (.i_reset(i_reset),.i_clk(i_clk).o_couterout(o_couterout).o_led(o_led));
+initial begin
+    i_reset=~i_reset;
+end 
+always@ (posedge i_clk)
+begin 
+end 
 endmodule
