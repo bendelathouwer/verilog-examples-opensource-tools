135c15ad01b7926bc0fe4055ddb0af2413e67a73
Ben <bendelathouwer@users.noreply.github.com>
2023-09-17 14:54:12 +0200

    testbench code
    added test bench code

--- clockdivider /clockdevider.v
+++ clockdivider /clockdevider.v
@@ -1,13 +1,11 @@
-module clockdivider (
-    input i_clk,i_reset,
-    output o_led,
-    output reg[7:0] counterout//number of outputs, clock gets divider  to the power of 2 (7^2 is the highest in this case )
+module clockdivider(
+    input clk, rst,
+    output reg [7:0] counterout
+    );
     
-);
-    always @ (posedge(i_clk),posedge (i_reset))
-    begin
-        if(i_reset) counterout <= 0 ;//if reset is high reset the counter
-        else counterout <= counterout + 1;
-         o_led = counterout[7];
-    end
+ always @ (posedge(clk), posedge(rst))
+ begin
+     if (rst) counterout <= 0;
+     else counterout <= counterout + 1;
+end
 endmodule
--- clockdivider /clockdevider_tb.v
+++ clockdivider /clockdevider_tb.v
@@ -1,12 +1,19 @@
 `timescale 1ns/1ps
 module clockdevider_tb(output wire o_led);
-reg i_clk,i_reset,o_couterout;
-clockdivider UUT (.i_reset(i_reset),.i_clk(i_clk).o_couterout(o_couterout).o_led(o_led));
-initial begin
-    i_reset=~i_reset;
-end 
-always@ (posedge i_clk)
-begin
-     
-end 
+    reg i_clk,i_reset,o_couterout;
+    clockdivider UUT (.reset(reset), 
+                      .clk(clk),
+                      .couterout(couterout));
+
+    always@*begin
+        reset= ~reset;
+        clk=0;
+        #20
+        clk=1;
+        #20
+    end 
+    initial begin
+        $dumpfile(" clockdevider_tb.vcb ");
+        $dumpvars(0,clockdevider_tb);
+    end
 endmodule
