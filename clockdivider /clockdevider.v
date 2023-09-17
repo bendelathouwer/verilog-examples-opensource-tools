17d71ef2ab588568d6cd85f29f6e7d44e2642c61
Ben <bendelathouwer@users.noreply.github.com>
2023-09-17 14:56:08 +0200

    Update clockdevider.v
    added blinkie light

--- clockdivider /clockdevider.v
+++ clockdivider /clockdevider.v
@@ -1,11 +1,12 @@
 module clockdivider(
     input clk, rst,
-    output reg [7:0] counterout
-    );
+    output reg [7:0] counterout,
+    output reg outputled );
     
  always @ (posedge(clk), posedge(rst))
  begin
      if (rst) counterout <= 0;
      else counterout <= counterout + 1;
+     assign outputled = couterout[7];
 end
 endmodule
