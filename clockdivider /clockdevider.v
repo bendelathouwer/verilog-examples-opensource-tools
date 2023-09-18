100d80bb1d2973f93f04799e074693c50cb3cbd4
Ben <bendelathouwer@users.noreply.github.com>
2023-09-18 18:38:39 +0200

    debugging test bench
    started debugging test bench

--- clockdivider /clockdevider.v
+++ clockdivider /clockdevider.v
@@ -1,12 +1,12 @@
 module clockdivider(
-    input clk, rst,
-    output reg [7:0] counterout,
-    output reg outputled );
+    input wire clk, rst,
+     output reg [7:0] counterout 
+     );
     
  always @ (posedge(clk), posedge(rst))
  begin
      if (rst) counterout <= 0;
      else counterout <= counterout + 1;
-     assign outputled = couterout[7];
+     //assign outputled = couterout[7];
 end
 endmodule
--- clockdivider /clockdevider_tb.v
+++ clockdivider /clockdevider_tb.v
@@ -1,16 +1,20 @@
 `timescale 1ns/1ps
-module clockdevider_tb(output wire o_led);
-    reg i_clk,i_reset,o_couterout;
-    clockdivider UUT (.reset(reset), 
-                      .clk(clk),
-                      .couterout(couterout));
+`default_nettype none
 
-    always @ * begin 
-        reset= ~reset;
+module clockdevider_tb(output wire o_led);
+    reg clk,rst;
+    reg [7:0] couterout;
+    clockdivider UUT (.rst(rst), 
+                      .clk(clk),
+                      .counterout(counterout));
+
+    always @ * 
+    begin 
+        rst= ~rst;
         clk=0;
-        #20
-        clk=1;
-        #20
+        #20;
+        clk = 1;
+        #20;
     end
     initial begin
         $dumpfile("clockdevider_tb.vcd");
